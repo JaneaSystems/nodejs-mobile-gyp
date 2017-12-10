b0d17311acf3c1d2d6f465e1937e287ceb6926bc
Jaime Bernardo <jaime@janeasystems.com>
2017-12-10 19:22:53 +0000

    ios: don't apply -mmacos-min-version for ios

--- gyp/pylib/gyp/xcode_emulation.py
+++ gyp/pylib/gyp/xcode_emulation.py
@@ -453,7 +453,6 @@ class XcodeSettings(object):
     return XcodeSettings._sdk_path_cache[sdk_root]
 
   def _AppendPlatformVersionMinFlags(self, lst):
-    self._Appendf(lst, 'MACOSX_DEPLOYMENT_TARGET', '-mmacosx-version-min=%s')
     if 'IPHONEOS_DEPLOYMENT_TARGET' in self._Settings():
       # TODO: Implement this better?
       sdk_path_basename = os.path.basename(self._SdkPath())
@@ -463,6 +462,9 @@ class XcodeSettings(object):
       else:
         self._Appendf(lst, 'IPHONEOS_DEPLOYMENT_TARGET',
                       '-miphoneos-version-min=%s')
+    else:
+      # Only use -mmacosx-version-min if it's not an iOS build.
+      self._Appendf(lst, 'MACOSX_DEPLOYMENT_TARGET', '-mmacosx-version-min=%s')
 
   def GetCflags(self, configname, arch=None):
     """Returns flags that need to be added to .c, .cc, .m, and .mm
